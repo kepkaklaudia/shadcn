0afb5fba25b72f2ca9d7221767b2e64487c8e509
kepkaklaudia <k.kepka@kratki.com>
2023-10-24 13:12:00 +0200

    common title added

---
+++ src/components/common/title.tsx
@@ -0,0 +1,15 @@
+import { MotionOpacity } from "@/components/motion/MotionOpacity";
+import { TitleProps } from "@/libs/types";
+
+export const Title = ({ heading, subheading }: TitleProps) => {
+  return (
+    <MotionOpacity>
+      <h2 className="lg:text-4xl xs:text-3xl text-2xl font-bold mt-12">
+        {heading}
+      </h2>
+      <h3 className="lg:text-lg text-sm mx-auto my-2.5 font-medium">
+        {subheading}
+      </h3>
+    </MotionOpacity>
+  );
+};
