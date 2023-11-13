ead5c500fc0c5a3bec532878a4e8a3d5a5e119d8
kepkaklaudia <k.kepka@kratki.com>
2023-11-13 13:12:00 +0100

    download content added

---
+++ src/app/[locale]/(pages)/downloads/content.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import Image from "next/image";
+import { useTranslations } from "next-intl";
+import { Intro } from "@/components/downloads/Intro";
+import { StrokedTitle } from "@/components/downloads/StrokedTitle";
+import { MotionHover } from "@/components/downloads/motion";
+
+export const Content = () => {
+  const t = useTranslations("downloads");
+
+  return (
+    <>
+      {/* <StrokedTitle title={t("Marketing materials")} />*/}
+      {/* <MarketingMaterials />*/}
+      <Intro />
+      <StrokedTitle title={"Logo"} />
+      <div className="flex justify-evenly w-[70%] gap-5 justify-items-center mx-auto my-20 md:my-[100px]">
+        <a
+          title="Open svg logo"
+          target="_blank"
+          rel="noopener noreferrer"
+          href={`/assets/downloads/logo-svg.svg`}
+          className="text-xs flex flex-col max-w-[200px] h-full gap-5 no-underline text-black md:text-sm lg:text-base"
+        >
+          <MotionHover>
+            <Image
+              className="w-full h-full max-w-[100px] max-h-[100px]"
+              width={100}
+              height={100}
+              alt=""
+              src={"/assets/downloads/svg.svg"}
+            />
+          </MotionHover>
+          <p className="font-medium text-xs xs:text-sm md:text-base">
+            {t("Logo")}
+          </p>
+        </a>
+        <a
+          title="Open sign book"
+          target="_blank"
+          rel="noopener noreferrer"
+          href={`/assets/downloads/logo-sign-book.pdf`}
+          className="text-xs flex flex-col max-w-[200px] h-full gap-5 no-underline text-black md:text-sm lg:text-base"
+        >
+          <MotionHover>
+            <Image
+              className="w-full h-full max-w-[100px] max-h-[100px]"
+              width={100}
+              height={100}
+              alt=""
+              src={"/assets/downloads/pdf.svg"}
+            />
+          </MotionHover>
+          <p className="font-medium text-xs xs:text-sm md:text-base">
+            {t("Sign book")}
+          </p>
+        </a>
+      </div>
+
+      {/*
+        <StrokedTitle title={t("Complaint form")} />
+        <div className={styles.container}>
+          <div className="text-xs flex flex-col max-w-[200px] h-full gap-5 no-underline text-black md:text-sm lg:text-base">
+            <MotionHover>
+              <Image
+                className="w-full h-full max-w-[100px] max-h-[100px]"
+                width={100}
+                height={100}
+                alt=""
+                src={"/assets/downloads/svg.svg"}
+              />
+            </MotionHover>
+            <p className="font-medium text-xs xs:text-sm md:text-base">{t("Logo")}</p>
+          </div>
+          <div className="text-xs flex flex-col max-w-[200px] h-full gap-5 no-underline text-black md:text-sm lg:text-base">
+            <MotionHover>
+              <Image
+                className="w-full h-full max-w-[100px] max-h-[100px]"
+                width={100}
+                height={100}
+                alt=""
+                src={"/assets/downloads/pdf.svg"}
+              />
+            </MotionHover>
+            <p className="font-medium text-xs xs:text-sm md:text-base">{t("Sign book")}</p>
+    </div> 
+        </div>*/}
+    </>
+  );
+};
