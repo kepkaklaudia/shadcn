3633166620195b2e463ebb2f776250fd4505f1ba
kepkaklaudia <kepkaklaudia@gmail.com>
2023-11-14 12:12:00 +0100

    classes added

---
+++ src/components/ui/classes.tsx
@@ -0,0 +1,8 @@
+export const linkStyle =
+  "cursor-pointer no-underline after:content-[''] after:block after:w-0 after:h-[0.5px] after:transition-all after:duration-300 after:m-auto hover:after:w-full hover:after:bg-white hover-opacity-100 text-sm xs:text-base font-normal text-black transition-colors duration-300  break-all text-left";
+
+export const heading =
+  "text-[25px] font-bold mt-[50px] mini:text-3xl lg:text-[35px]";
+
+export const homepageWrapper =
+  "lg:grid-cols-[1fr_1fr] lg:gap-[60px] lg:mx-auto lg:my-[50px] w-4/5 grid-cols-[1fr] text-left grid text-sm gap-5 items-center mx-auto my-[30px]";
