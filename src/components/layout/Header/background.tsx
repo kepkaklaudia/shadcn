ca120368a7b90095d28e77f561a319217730e46a
kepkaklaudia <kepkaklaudia@gmail.com>
2023-11-25 12:12:00 +0100

    background added

---
+++ src/components/layout/Header/background.tsx
@@ -0,0 +1,7 @@
+export const Background = () => (
+  <div className="bg-alabaster w-full flex h-auto justify-center z-[1031] relative mt-[61px] xs:mb-0 xs:mt-[66px] xs:mb-[30px]  md:mt-0 md:mb-20 sm:mt-[71px] mx-auto z-[1000] md:z-[1031]">
+    <video className="w-full h-auto" autoPlay playsInline loop muted>
+      <source src="assets/layout/balhanger.mp4" type="video/mp4" />
+    </video>
+  </div>
+);
