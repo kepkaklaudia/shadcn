d5d883029f07e76d9a085331dc892afa28a51dd5
kepkaklaudia <kepkaklaudia@gmail.com>
2023-12-29 12:12:00 +0100

    properties added

---
+++ src/components/product/properties.tsx
@@ -0,0 +1,33 @@
+import { useTranslations } from "next-intl";
+
+interface PropertiesProps {
+  standard: string[];
+}
+
+export const Properties = ({ standard }: PropertiesProps) => {
+  const t = useTranslations("product.properties");
+
+  return (
+    <div>
+      {standard.length > 0 ? (
+        <div className="text-sm bg-white border text-left pb-2.5 p-5 rounded-md border-solid border-alto sm:max-w-none max-w-[700px] w-4/5 sm:w-auto  mt-[30px] mb-0 mx-auto xs:mr-[50px]">
+          <p className="text-lg font-medium uppercase mb-[5px] sm:text-xl sm:mb-0">
+            {t("Properties")}
+          </p>
+          <ul className="w-fit list-none pl-2.5">
+            {standard.map((item: string, index: number) => (
+              <li
+                className="grid grid-cols-[auto_1fr] items-center before:content-[''] before:flex before:self-start before:h-[15px] before:w-[15px] before:bg-[url('/assets/carouselArrow.svg')] before:bg-contain before:bg-no-repeat before:mt-1"
+                key={index}
+              >
+                {item}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
