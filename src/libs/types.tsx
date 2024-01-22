b9661ac5d9ec7b1a33a1884f6b3fc3d264c2af68
kepkaklaudia <kepkaklaudia@gmail.com>
2024-01-22 12:12:00 +0100

    types added

--- src/libs/types.tsx
+++ src/libs/types.tsx
@@ -1,4 +1,10 @@
 import { ReactNode } from "react";
+import {
+  Control,
+  FieldErrors,
+  FieldValues,
+  UseFormRegister,
+} from "react-hook-form";
 
 export interface LayoutProps {
   children: ReactNode;
@@ -26,6 +32,67 @@ export interface CategoryButtonProps {
 export interface StrokeProps {
   additionalClass?: string;
   src: string;
+}
+
+export interface ModalItem {
+  label: string;
+  value: string;
+}
+
+export interface StyledModalProps {
+  show: boolean;
+  setShow: React.Dispatch<React.SetStateAction<boolean>>;
+  modalItems: ModalItem[];
+  data: FieldValues | null;
+  isHome?: boolean;
+}
+export interface TextFieldProps {
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors<FieldValues>;
+  fieldName: string;
+  label: string;
+  placeholder: string;
+  type: string;
+  minLength?: { value: number; message: string };
+  requiredMessage?: string;
+  validateFunction?: (value: string) => boolean | string;
+}
+
+export interface CheckboxFieldProps {
+  errors: FieldErrors<FieldValues>;
+  fieldName: string;
+  label: string;
+  requiredMessage?: string;
+  control: Control;
+  onClick: () => void;
+}
+
+export interface DropzoneProps {
+  filesArray: File[];
+  setFilesArray: (files: File[]) => void;
+  mainLabel: string;
+  loadedLabel: string;
+  emptyLabel: string;
+  loadedTitle: string;
+  emptyTitle: string;
+  deleteTitle: string;
+  deleteAll: string;
+}
+
+export interface TextareaFieldProps {
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors<FieldValues>;
+  fieldName: string;
+  placeholder: string;
+  requiredMessage?: string;
+  validateFunction?: any;
+  label: string;
+}
+
+export interface ErrorParagraphProps {
+  errors: FieldErrors<FieldValues>;
+  fieldName: string;
+  className?: string;
 }
 
 // about-us
@@ -204,7 +271,6 @@ export interface SortSectionProps {
   setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-
 //homepage
 export interface CarouselProps {
   images: string[];
@@ -250,4 +316,151 @@ export interface MotionHoverProps {
   rotateY: number;
   skewY: number;
   verticalBox: string;
+}
+
+//product
+export interface HeaderProductProps {
+  path: string;
+  name: string;
+  category: keyof Messages["products"]["heading"];
+}
+
+export interface ProductData {
+  data: {
+    pathID: string;
+    pdfGalvanized: string;
+    pdfPowdered: string;
+    name: string;
+    category: keyof Messages["products"]["heading"];
+    path: string;
+    family: string;
+    tags: keyof Messages["product"][];
+  };
+  specification: {
+    gvw: string;
+    load: string;
+    area: string;
+    weight: string;
+    dimensions: string;
+    axles: string;
+    axlesType: string;
+    wheels: string;
+  };
+  drawings: {
+    profile: string;
+    front: string;
+    top: string;
+  };
+  standard: string[];
+  optional: string[];
+  images: {
+    galvanized: string[];
+    powdered: string[];
+    filled: string[];
+  };
+}
+
+export interface PropertiesProductProps {
+  standard: string[];
+}
+
+export interface VariantButtonProps {
+  type: string;
+  label: keyof Messages["product"]["productDetails"]["variantButtons"];
+  setVariant: React.Dispatch<React.SetStateAction<string>>;
+  images: {
+    galvanized: string[];
+    powdered: string[];
+    filled: string[];
+  };
+  path: string;
+  buttonType: string;
+}
+
+export interface VariantButtonsProps {
+  path: string;
+  setVariant: React.Dispatch<React.SetStateAction<string>>;
+  images: {
+    galvanized: string[];
+    powdered: string[];
+    filled: string[];
+  };
+}
+
+export interface TagsProductProps {
+  path: string;
+  tags: keyof Messages["product"]["tags"][];
+  locale: string;
+}
+
+export interface ProductCarouselProps {
+  family: string;
+  variant: string;
+  url: {
+    [variant: string]: string[];
+  };
+}
+
+export interface DownloadsProps {
+  pdfGalvanized: string;
+  pdfPowdered: string;
+}
+
+export interface DrawingsProps {
+  pathID: string;
+  profile: string;
+  front: string;
+  top: string;
+}
+
+export interface MenuButtonsProps {
+  setActiveSection: React.Dispatch<React.SetStateAction<string>>;
+  activeSection: string;
+}
+
+export interface MotionContentProps {
+  children: ReactNode;
+  ownKey: string;
+}
+
+export interface TableRowProps {
+  label: string | JSX.Element;
+  value: string | JSX.Element;
+}
+
+interface Specification {
+  gvw: string;
+  load: string;
+  area: string;
+  weight: string;
+  dimensions: string;
+  axles: string;
+  axlesType: string;
+  wheels: string;
+}
+
+export interface TechnicalSpecificationsProps {
+  specification: Specification;
+}
+
+export interface ProductDetailsProps {
+  specification: {
+    gvw: string;
+    load: string;
+    area: string;
+    weight: string;
+    dimensions: string;
+    axles: string;
+    axlesType: string;
+    wheels: string;
+  };
+  optional: string[];
+  pathID: string;
+  pdfPowdered: string;
+  pdfGalvanized: string;
+  drawings: {
+    profile: string;
+    front: string;
+    top: string;
+  };
 }
